72d7d1c8e50e52731bef44994051261c17bf31ef
Jack <gskwon@maxst.com>
2017-12-07 17:51:42 +0900

    Revert "1. Apply 3.3.0 SDK"
    This reverts commit dcc51306ccebb673cdf0e4b15c3feffc6425db03.

---
+++ MaxstAR/src/main/java/com/thej/newar/util/SampleUtil.java
@@ -0,0 +1,9 @@
+package com.thej.newar.util;
+
+
+public class SampleUtil {
+
+	public static final String PREF_NAME = "pref";
+	public static final String PREF_KEY_CAM_RESOLUTION = "cam_resolution";
+
+}
