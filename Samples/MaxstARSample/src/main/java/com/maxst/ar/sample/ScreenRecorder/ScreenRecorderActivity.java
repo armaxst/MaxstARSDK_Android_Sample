8e5c06ccb0f6e7c59128da195b47f21951e50f0b
Charles <shok@maxst.com>
2018-04-13 11:43:07 +0900

    Animation Button bug fix

--- Samples/MaxstARSample/src/main/java/com/maxst/ar/sample/ScreenRecorder/ScreenRecorderActivity.java
+++ Samples/MaxstARSample/src/main/java/com/maxst/ar/sample/ScreenRecorder/ScreenRecorderActivity.java
@@ -1,8 +1,11 @@
 package com.maxst.ar.sample.ScreenRecorder;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.RequiresApi;
 import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +15,7 @@ import android.widget.ImageView;
 import com.maxst.ar.sample.R;
 import com.maxst.screenrecorder.ScreenRecorder;
 
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class ScreenRecorderActivity extends AppCompatActivity {
 
 	private static final String TAG = ScreenRecorderActivity.class.getSimpleName();
@@ -59,21 +63,33 @@ public class ScreenRecorderActivity extends AppCompatActivity {
 			 * and then, start to record!
 			 */
 			recorder = new ScreenRecorder(ScreenRecorderActivity.this, data);
+			recorder.setDstPath(Environment.getExternalStoragePublicDirectory("Movies").getAbsolutePath() + "/test.mp4");
 			recorder.record();
 		} else if (requestCode == REQ_RECORD) {
 			setRecordButton(true);
 		}
 	}
 
+
 	private void setRecordButton(boolean isStopped) {
 		if (isStopped) {
 			record.setImageResource(R.drawable.stop_to_record);
-			((AnimatedVectorDrawableCompat) record.getDrawable()).start();
+			startAnimatedVectorDrawable(record.getDrawable());
 			record.setTag(null);
 		} else {
 			record.setImageResource(R.drawable.record_to_stop);
-			((AnimatedVectorDrawableCompat) record.getDrawable()).start();
+			startAnimatedVectorDrawable(record.getDrawable());
 			record.setTag(0);
+		}
+
+
+	}
+
+	private void startAnimatedVectorDrawable(Drawable drawable) {
+		if (drawable instanceof AnimatedVectorDrawable) {
+			((AnimatedVectorDrawable) record.getDrawable()).start();
+		} else if (drawable instanceof AnimatedVectorDrawableCompat) {
+			((AnimatedVectorDrawableCompat) record.getDrawable()).start();
 		}
 	}
 }
